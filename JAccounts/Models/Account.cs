a648f3041571ee35afbd9abda947e3d2c2f63854
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-05-14 11:01:51 +0200

    Add Account

---
+++ JAccounts/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace JAccounts.Models
+{
+    public class Account
+    {
+        public uint AccountID { get; set; }
+        public string Name { get; set; }
+        public string BIC { get; set; }
+        public string IBAN { get; set; }
+        public bool IsActive { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime OpenedOn { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? ClosedOn { get; set; }
+    }
+}
